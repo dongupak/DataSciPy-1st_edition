19f6743adb0e712973ddb909a381fdf2c093dc78
dongupak <dongupak@gmail.com>
2020-09-23 15:13:00 +0900

    add src

---
+++ src/Ch01/lab1_2.py
@@ -0,0 +1,11 @@
+import turtle
+t = turtle.Turtle()
+t.shape("turtle")
+
+t.forward(100)
+t.left(120)
+t.forward(100)
+t.left(120)
+t.forward(100)
+
+t.done()
---
+++ src/Ch02/lab2_3.py
@@ -0,0 +1,21 @@
+import turtle
+
+t = turtle.Turtle()
+t.shape('turtle')
+
+radius = 100
+t.circle(radius)           # 반지름이 100인 원을 그린다.
+radius = 200
+t.circle(radius) 
+radius = 50
+t.circle(radius) 
+
+t.left(90)
+radius = 100
+t.circle(radius)           # 반지름이 100인 원을 그린다.
+radius = 200
+t.circle(radius) 
+radius = 50
+t.circle(radius) 
+
+t.done()
