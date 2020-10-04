9acb138abec56c965243701e717f32b59877b56a
DongGyu Park <dongupak@gmail.com>
2020-10-04 14:36:15 +0900

    update file name

--- src/Ch13/code_13_3.py
+++ src/Ch13/code_13_3.py
@@ -6,4 +6,6 @@ img_color = cv2.imread('d:/data/mandrill.png', cv2.IMREAD_COLOR)
 cv2.imshow('grayscale', img_gray)    # 맨드릴 원숭이를 회색조 이미지로 화면에 표시
 cv2.imshow('color image', img_color) # 맨드릴 원숭이를 컬러 이미지로 화면에 표시
 
-#cv2.waitKey(0)
+# 다음 두 행은 키보드 입력을 기다렸다가 모든 창을 끄고 종료하는 코드
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ src/Ch13/code_13_4_1.py
@@ -0,0 +1,9 @@
+import cv2
+img = cv2.imread('d:/data/mandrill.png', 1)
+cv2.line(img, (0,0), (200,200), (0,0,255), 5)    # 직선의 시작점과 끝점, 색상, 두께를 지정함
+cv2.rectangle(img, (0,200), (200,20), (0,0,0), 5)  # 사각형의 좌표, 색상을 지정
+
+cv2.imshow('lined', img)
+# 다음 두 행은 키보드 입력을 기다렸다가 모든 창을 끄고 종료하는 코드
+cv2.waitKey(0)
+cv2.destroyAllWindows()
