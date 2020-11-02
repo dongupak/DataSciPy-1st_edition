7cc5d76fa4733b1ba629142d343ffc5faffc5c6c
dongupak <dongupak@gmail.com>
2020-11-02 12:23:52 +0900

    add lab

--- src/Ch13/code_13_4_1.py
+++ src/Ch13/code_13_4_1.py
@@ -1,9 +1,10 @@
 import cv2
+
 img = cv2.imread('d:/data/mandrill.png', 1)
 cv2.line(img, (0,0), (200,200), (0,0,255), 5)    # 직선의 시작점과 끝점, 색상, 두께를 지정함
-cv2.rectangle(img, (0,200), (200,20), (0,0,0), 5)  # 사각형의 좌표, 색상을 지정
-
+cv2.arrowedLine(img, (0,200),(200,20),(0,0,255), 5) #화살표의 시작점, 끝점, 색상, 두께지정
 cv2.imshow('lined', img)
-# 다음 두 행은 키보드 입력을 기다렸다가 모든 창을 끄고 종료하는 코드
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+
+# 다음 두 행은 키보드 입력을 기다렸다가 모든 창을 끄고 종료하는 코드, 향후 코드에서는 생략함
+cv2.waitKey(0)                       # 사용자 입력을 기다림
+cv2.destroyAllWindows()              # 모든 창을 없애고 프로그램 종료
--- src/Ch13/lab_13_1.py
+++ src/Ch13/lab_13_1.py
@@ -4,10 +4,10 @@ import cv2
 img1 = cv2.imread('d:/data/green_back.png')       # 전경 이미지 읽기
 img2 = cv2.imread('d:/data/iceberg.png')          # 배경 이미지 읽기
 
-mask_image = cv2.resize(img1, (300, 400))
+front_image = cv2.resize(img1, (300, 400))
 back_image = cv2.resize(img2, (300, 400))
 
-img_hsv = cv2.cvtColor(img1, cv2.COLOR_BGR2HSV)   # HSV 공간으로 옮김
+img_hsv = cv2.cvtColor(front_image, cv2.COLOR_BGR2HSV)   # HSV 공간으로 옮김
 l_bound = np.array([40, 100, 50])                 # 녹색 색상의 하한
 u_bound = np.array([80, 255, 255])                # 녹색 색상의 상한
 
@@ -15,9 +15,9 @@ my_mask  = cv2.inRange(img_hsv, l_bound, u_bound)   # 녹색 픽셀 찾기
 mask_inv = cv2.bitwise_not(my_mask)                 # 녹색이 아닌 픽셀 찾기
 
 # 녹색 픽셀들만 추출하기
-extracted = cv2.bitwise_and(mask_image, mask_image, mask = my_mask) 
+extracted = cv2.bitwise_and(front_image, front_image, mask = my_mask) 
 # 녹색 아닌 픽셀만 추출하기
-removed = cv2.bitwise_and(mask_image, mask_image, mask = mask_inv)   
+removed = cv2.bitwise_and(front_image, front_image, mask = mask_inv)   
 # 녹색과 겹치는 배경 추출
 background = cv2.bitwise_and(back_image, back_image, mask = my_mask)   
 # 녹색 제거 전경 + 배경   
@@ -26,4 +26,9 @@ merged = cv2.bitwise_or(removed, background)
 cv2.imshow('mask', my_mask)
 cv2.imshow('mask_inv', mask_inv)
 cv2.imshow('removed', removed)
+cv2.imshow('background', background)
 cv2.imshow('merged', merged)
+
+# 다음 두 행은 키보드 입력을 기다렸다가 모든 창을 끄고 종료하는 코드로 향후 코드에서는 생략함
+cv2.waitKey(0)                       # 사용자 입력을 기다림
+cv2.destroyAllWindows()              # 모든 창을 없애고 프로그램 종료
