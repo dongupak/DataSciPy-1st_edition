0961bb0fd28aac9e8d74f1026c31a7722cc47a01
dongupak <dongupak@gmail.com>
2020-11-02 11:56:41 +0900

    add lab code

--- src/Ch13/code_13_7.py
+++ src/Ch13/code_13_7.py
@@ -7,12 +7,12 @@ image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 blue_low = np.array([80, 0, 0])
 blue_high = np.array([130, 255, 255])
 
-mask = cv2.inRange(image_hsv, blue_low, blue_high)
+my_mask = cv2.inRange(image_hsv, blue_low, blue_high) 
 
 cv2.imshow('original', image)
-cv2.imshow('mask', mask)
+cv2.imshow('mask', my_mask)
 
-extracted = cv2.bitwise_and(image, image, mask=mask)
+extracted = cv2.bitwise_and(image, image, mask=my_mask)
 cv2.imshow('extracted', extracted)
 
 # 다음 두 행은 키보드 입력을 기다렸다가 모든 창을 끄고 종료하는 코드
