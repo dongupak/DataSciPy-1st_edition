1170821d5f4e755e83c47fbef2bf6abd302010f5
khojoooo <hojoon1619@gmail.com>
2021-01-07 15:32:26 +0900

    Comment edit(LAB## -> LAB ##)

--- src/Ch12/lab_12_3.py
+++ src/Ch12/lab_12_3.py
@@ -1,6 +1,6 @@
 #
 # 따라하며 배우는 파이썬과 데이터과학(생능출판사 2020)
-# LAB12-3 울릉도는 몇 월에 바람이 가장 강할까? - groupby() 활용, 324쪽
+# LAB 12-3 울릉도는 몇 월에 바람이 가장 강할까? - groupby() 활용, 324쪽
 #
 import pandas as pd
 import matplotlib.pyplot as plt
